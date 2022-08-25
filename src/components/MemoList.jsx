55514ffa676d88ac6d9fc2f234d82c3f45db5369
sho-s-git <rikotai28@icloud.com>
2022-08-26 00:34:27 +0900

    Make components touchable

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,6 +1,6 @@
 import { Feather } from '@expo/vector-icons';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function MemoList() {
   return (
@@ -10,9 +10,9 @@ export default function MemoList() {
           <Text style={styles.memoListItemTitle}>買い物リスト</Text>
           <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
         </View>
-        <View>
+        <TouchableOpacity>
           <Feather name="x" size={16} color="#B0B0B0" />
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
